8d02ca139bcae445a9ac5969324cf03a5794bd99
Yi Wei <ywei32@fazel-du-0.cs.washington.edu>
2026-02-04 03:21:33 -0800

    Stop tracking Nash_Equilibrium

--- Nash_Equilibrium/src/algorithms/utils/helper.py
+++
@@ -1,8 +0,0 @@
-def construct_block_range(begin: int, end: int, block_size):
-    blocks = []
-
-    for start in range(begin, end, block_size):
-        stop = min(start + block_size, end)  # Ensure the end doesn't exceed total columns
-        block_range = range(start, stop)
-        blocks.append(block_range)
-    return blocks
